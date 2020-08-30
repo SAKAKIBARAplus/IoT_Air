8150a221b99004592952e773dbc49b37cb72f81b
SAKAKIBARAplus <ususio1192o157@yahoo.co.jp>
2020-08-30 18:21:48 +0900

    no message

--- index.php
+++ index.php
@@ -10,7 +10,10 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 //LINE Message APIがリクエストに付与した署名を取得
-$signature = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+//書面が正当化チェック。正当であればリクエストをぱーすし配列へ
+$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 
 //配列に格納された各イベントをループで処理
 foreach ($events as $events){
