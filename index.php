d5cfe5cc3535d470804b1de87c39cb60dc1ae6e0
SAKAKIBARAplus <ususio1192o157@yahoo.co.jp>
2020-08-30 10:54:20 +0900

    no message

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Composerでインストールしたライブラリを一括読み取り
-require_once __DIR__. '/Vender/autoload.php';
+require_once __DIR__ . '/Vender/autoload.php';
 
 //POSTメソッドで返される値を取得、表示
 $inputString = file_get_contents('php://input');
